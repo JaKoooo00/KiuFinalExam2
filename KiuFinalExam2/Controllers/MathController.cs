b6d3ceaa87f07e08356488e368739f217d77fcad
giorgi jakonia <jakonia.g.16@gmail.com>
2025-06-25 21:54:51 +0400

    Add project files.

---
+++ KiuFinalExam2/Controllers/MathController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KiuFinalExam2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpPost]
+        public double Square([FromForm] double x,[FromForm] double y)
+        {
+            return x * x + y * y;
+        }
+    }
+}
